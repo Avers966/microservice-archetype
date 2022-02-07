a096bf3a0dfa41b95087c2644ecb78ec601af488
Холмогоров Денис <dendiesel88@yandex.ru>
2022-02-07 15:38:02 +0000

    Создание архитипа

--- src/main/resources/archetype-resources/impl/src/main/java.ru.skillbox.diplom.group25.__artifactIdForPackage__/Application.java
+++ src/main/resources/archetype-resources/impl/src/main/java.ru.skillbox.diplom.group25.__artifactIdForPackage__/Application.java
@@ -1,4 +1,4 @@
-package ru.skillbox.diplom.alpha;
+package ru.skillbox.diplom.group25;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
